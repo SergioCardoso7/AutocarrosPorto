5b6ea2c52c04c4ea4e5d8c79e10a8f57fc070514
Sergio <1210891@isep.ipp.pt>
2025-02-19 22:59:00 +0000

    Initial project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.sergio.portobus"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sergio.portobus"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    // ViewModelScope
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
---
+++ app/src/main/java/com/sergio/portobus/PortoBusApplication.kt
@@ -0,0 +1,9 @@
+package com.sergio.portobus
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class PortoBusApplication : Application() {
+}
